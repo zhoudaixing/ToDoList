c02ae674b9703ac7f035b62f4e2afd14a16c4436
zhoudaixing-wsl <zhoudaixing@gmail.com>
2024-03-01 00:18:48 +0800

    add finishList

--- src/layouts/List/index.tsx
+++ src/layouts/List/index.tsx
@@ -1,30 +1,46 @@
 import Box from "@mui/material/Box";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
-import Divider from "@mui/material/Divider";
+
+// import ListItemButton from "@mui/material/ListItemButton";
+// import ListItemIcon from "@mui/material/ListItemIcon";
+// import ListItemText from "@mui/material/ListItemText";
+// import Divider from "@mui/material/Divider";
 // import InboxIcon from "@mui/icons-material/Inbox";
 // import DraftsIcon from "@mui/icons-material/Drafts";
 
 import useStore from "@/store";
-
 const ListPage = () => {
-  const { list } = useStore();
+  // const { deleteItem } = useStore();
+
+  const { list, deleteItem, finishList, finishItem } = useStore();
   console.log(list);
+  const onClick = (id: number) => {
+    finishItem(id);
+  };
+  const onDelete = (id: number, list: 'finishList') => {
+    deleteItem(id, list)
+  }
   return (
     <div className="p-4">
       <Box sx={{ width: "100%", bgcolor: "background.paper" }}>
         <nav aria-label="main mailbox folders">
           <List>
-            {list.map(item => (
-              <ListItem>{item}</ListItem>
+            {list.map((item) => (
+              <ListItem onClick={() => onClick(item.id)}>
+                {item.content}
+              </ListItem>
+            ))}
+          </List>
+          <div className="bg-blue-500 h-10" />
+          <List>
+          {finishList.map((item) => (
+              <ListItem onClick={() => onDelete(item.id, 'finishList')}>
+                {item.content}
+              </ListItem>
             ))}
           </List>
         </nav>
-
-
       </Box>
     </div>
   );
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,13 +1,54 @@
-import { create } from 'zustand'
+import { create } from "zustand";
+
+interface ListItem {
+  id: number;
+  content: string;
+  // time?: Date | undefined
+  state?: 0 | 1 | 2;
+  createdTime?: Date | undefined;
+  finishedTime?: Date | undefined;
+  creater?: string | undefined;
+  tag?: string[]
+  category?: string[]
+}
 
 interface State {
-  list: string[],
-  addList: (todo: string) => void
+  count: number;
+  list: ListItem[];
+  finishList: ListItem[];
+  addList: (todo: string) => void;
+  deleteItem: (id: number, list: 'list' | 'finishList') => void;
+  finishItem: (id: number) => void;
 }
 
 const useStore = create<State>((set) => ({
-  list: ['123', '456'],
-  addList: (todo) => set((state) => ({ list: [...state.list, todo] })),
-}))
+  count: 2,
+  list: [
+    {
+      id: 1,
+      content: "123",
+    },
+    {
+      id: 2,
+      content: "456",
+    },
+  ],
+  finishList: [],
+  addList: (todo) =>
+    set((state) => ({
+      list: [...state.list, { id: state.count + 1, content: todo }],
+      count: state.count + 1,
+    })),
+  deleteItem: (id, list) =>
+    set((state) => ({ finishList: state[list].filter((item) => item.id !== id) })),
+  finishItem: (id) =>
+    set((state) => {
+      const item = state.list.filter((item) => item.id === id);
+      return {
+        list: state.list.filter((item) => item.id !== id),
+        finishList: [...state.finishList, ...item],
+      };
+    }),
+}));
 
-export default useStore
+export default useStore;
